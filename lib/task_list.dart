291f77edb18ba71ad05eba9f3a94ab918e7b1101
Wesley Fuchter <wesley.fuchter@gmail.com>
2021-08-23 15:23:41 -0300

    onTap stup and testing

--- lib/task_list.dart
+++ lib/task_list.dart
@@ -7,10 +7,6 @@ class TaskList extends StatelessWidget {
   final List<String> tasks;
   final Function onRemove;
 
-  void onTap(String content) {
-    onRemove(content);
-  }
-
   @override
   Widget build(BuildContext context) {
     final tiles = tasks
@@ -19,10 +15,9 @@ class TaskList extends StatelessWidget {
             key: Key(task),
             title: Text(task),
             onTap: () {
-              onTap(task);
+              onRemove(task);
             }))
         .toList(growable: true);
-
     return ListView(padding: const EdgeInsets.all(8), children: tiles);
   }
 }
--- test/task_list_test.dart
+++ test/task_list_test.dart
@@ -1,25 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_testing_experiment/task_list.dart';
+import 'package:mockito/mockito.dart';
+
+class MockFunction extends Mock implements Function {
+  void call(String task);
+}
 
 void main() {
-  testWidgets('it should render the list of tasks',
-      (WidgetTester tester) async {
-    const tasks = ['Task 1', 'Task 2', 'Task 3'];
-
-    await tester.pumpWidget(MaterialApp(
-      home: Material(
-        child: Directionality(
-          textDirection: TextDirection.ltr,
-          child: Center(
-            child: TaskList(tasks: tasks, onRemove: () {}),
+  group('TaskList', () {
+    testWidgets('Should render the collection of tasks',
+        (WidgetTester tester) async {
+      const tasks = ['Task 1', 'Task 2', 'Task 3'];
+      await tester.pumpWidget(MaterialApp(
+        home: Material(
+          child: Directionality(
+            textDirection: TextDirection.ltr,
+            child: Center(
+              child: TaskList(tasks: tasks, onRemove: () {}),
+            ),
           ),
         ),
-      ),
-    ));
-
-    expect(find.text('Task 1'), findsOneWidget);
-    expect(find.text('Task 2'), findsOneWidget);
-    expect(find.text('Task 3'), findsOneWidget);
+      ));
+      expect(find.text('Task 1'), findsOneWidget);
+      expect(find.text('Task 2'), findsOneWidget);
+      expect(find.text('Task 3'), findsOneWidget);
+    });
+    testWidgets('Should call the onRemove function',
+        (WidgetTester tester) async {
+      const tasks = ['Task 1', 'Task 2', 'Task 3'];
+      final onRemove = MockFunction();
+      await tester.pumpWidget(MaterialApp(
+        home: Material(
+          child: Directionality(
+            textDirection: TextDirection.ltr,
+            child: Center(
+              child: TaskList(tasks: tasks, onRemove: onRemove),
+            ),
+          ),
+        ),
+      ));
+      verifyNever(onRemove('Task 1'));
+      final taskItem = find.text('Task 1');
+      expect(taskItem, findsOneWidget);
+      await tester.tap(taskItem);
+      await tester.pump();
+      verify(onRemove('Task 1')).called(1);
+    });
   });
 }
